4d39c698a7c14f30ff38fc1512526a520f22b25d
Nafiz Al din <siriusal121211@gmail.com>
2020-11-07 21:54:31 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function () {
+    
+    $('.banner_part_slide').carousel({
+        interval: 1700
+      })
+    $('.testimonial_slide').carousel({
+        interval: 1000
+      })
+      
+
+
+
+
+
+})
+
+
+var filterizd = $('.filter_gallery').filterizr({
+       //options object
+    });
